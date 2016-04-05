0deb52ed5990d740770308ccea50e9ac30c0fcc3
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2016-04-05 19:37:41 +0100

    Scene builder deleted

--- src/main/java/norvidi/CMan.java
+++ src/main/java/norvidi/CMan.java
@@ -1,18 +1,48 @@
 package norvidi;
 
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.util.List;
+
+import static norvidi.DriveQuickstart.getDriveService;
 
 public class CMan extends Application {
 
+    TableView table;
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("main.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
+        Drive service = getDriveService();
+
+        // Print the names and IDs for up to 10 files.
+        FileList result = service.files().list()
+                .setPageSize(10)
+                .setFields("nextPageToken, files(id, name)")
+                .execute();
+        List<File> files = result.getFiles();
+        if (files == null || files.size() == 0) {
+            System.out.println("No files found.");
+        } else {
+            System.out.println("Files:");
+            for (File file : files) {
+                System.out.printf("%s (%s)\n", file.getName(), file.getId());
+            }
+        }
+        table = new TableView();
+        StackPane layout = new StackPane();
+        layout.getChildren().add(table);
+
+        Scene scene = new Scene(layout, 1250, 750);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
