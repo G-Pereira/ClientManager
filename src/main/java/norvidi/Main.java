b029d9cf08ec0ade56fefe930c547fce3fb3729f
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2016-04-14 19:52:25 +0100

    Drive Letdown

--- src/main/java/norvidi/Main.java
+++ src/main/java/norvidi/Main.java
@@ -1,41 +1,127 @@
 package norvidi;
 
-import com.google.api.services.drive.Drive;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.OutputStream;
-
-import static norvidi.GDrive.getDriveService;
-
-
+import java.net.URL;
+import java.net.URLConnection;
 
 public class Main extends Application {
 
-    static TableView tbl;
-
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Norvidi Client Manager");
 
-        tbl = new TableView();
+        TableView tbl = new TableView();
         StackPane sp = new StackPane();
         sp.getChildren().add(tbl);
 
         Scene scn = new Scene(sp, 750, 500);
         primaryStage.setScene(scn);
         primaryStage.show();
+        httpConn();
+    }
 
-        // Build a new authorized API client service.
-        Drive service = getDriveService();
+    private final String CrLf = "\r\n";
 
-        String fileId = "0B8USYnKeDsgoN0RMT0dIQXZtS0E";
-        OutputStream outputStream = new ByteArrayOutputStream();
-        service.files().export(fileId, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet").executeMediaAndDownloadTo(outputStream);
+    private void httpConn() {
+        URLConnection conn = null;
+        OutputStream os = null;
+        InputStream is = null;
+
+        try {
+            URL url = new URL("http://norvidi.tk/up.php");
+            System.out.println("url:" + url);
+            conn = url.openConnection();
+            conn.setDoOutput(true);
+
+            String postData = "";
+
+            InputStream imgIs = getClass().getResourceAsStream("/test.jpg");
+            byte[] imgData = new byte[imgIs.available()];
+            imgIs.read(imgData);
+
+            String message1 = "";
+            message1 += "-----------------------------4664151417711" + CrLf;
+            message1 += "Content-Disposition: form-data; name=\"uploadedfile\"; filename=\"test.jpg\""
+                    + CrLf;
+            message1 += "Content-Type: image/jpeg" + CrLf;
+            message1 += CrLf;
+
+            // the image is sent between the messages in the multipart message.
+
+            String message2 = "";
+            message2 += CrLf + "-----------------------------4664151417711--"
+                    + CrLf;
+
+            conn.setRequestProperty("Content-Type",
+                    "multipart/form-data; boundary=---------------------------4664151417711");
+            // might not need to specify the content-length when sending chunked
+            // data.
+            conn.setRequestProperty("Content-Length", String.valueOf((message1
+                    .length() + message2.length() + imgData.length)));
+
+            System.out.println("open os");
+            os = conn.getOutputStream();
+
+            System.out.println(message1);
+            os.write(message1.getBytes());
+
+            // SEND THE IMAGE
+            int index = 0;
+            int size = 1024;
+            do {
+                System.out.println("write:" + index);
+                if ((index + size) > imgData.length) {
+                    size = imgData.length - index;
+                }
+                os.write(imgData, index, size);
+                index += size;
+            } while (index < imgData.length);
+            System.out.println("written:" + index);
+
+            System.out.println(message2);
+            os.write(message2.getBytes());
+            os.flush();
+
+            System.out.println("open is");
+            is = conn.getInputStream();
+
+            char buff = 512;
+            int len;
+            byte[] data = new byte[buff];
+            do {
+                System.out.println("READ");
+                len = is.read(data);
+
+                if (len > 0) {
+                    System.out.println(new String(data, 0, len));
+                }
+            } while (len > 0);
+
+            System.out.println("DONE");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Close connection");
+            try {
+                os.close();
+            } catch (Exception e) {
+            }
+            try {
+                is.close();
+            } catch (Exception e) {
+            }
+            try {
+
+            } catch (Exception e) {
+            }
+        }
     }
 
     public static void main(String[] args) {
