2540f1e16313938338b6f88319059dd7bc6f5483
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2016-04-15 11:51:43 +0100

    read excel from the web

--- src/main/java/norvidi/Main.java
+++ src/main/java/norvidi/Main.java
@@ -6,10 +6,15 @@ import javafx.scene.control.TableView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Iterator;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 
 public class Main extends Application {
 
@@ -26,6 +31,51 @@ public class Main extends Application {
         primaryStage.show();
         upFile upit = new upFile();
         upit.httpConn();
+
+        try {
+
+            FileInputStream file = new FileInputStream(new File("http://norvidi.tk/norvidi.xlsx"));
+
+            //Get the workbook instance for XLS file
+            HSSFWorkbook workbook = new HSSFWorkbook(file);
+
+            //Get first sheet from the workbook
+            HSSFSheet sheet = workbook.getSheetAt(0);
+
+            //Iterate through each rows from first sheet
+            Iterator<Row> rowIterator = sheet.iterator();
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+
+                //For each row, iterate through each columns
+                Iterator<Cell> cellIterator = row.cellIterator();
+                while (cellIterator.hasNext()) {
+
+                    Cell cell = cellIterator.next();
+
+                    switch (cell.getCellType()) {
+                        case Cell.CELL_TYPE_BOOLEAN:
+                            System.out.print(cell.getBooleanCellValue() + "\t\t");
+                            break;
+                        case Cell.CELL_TYPE_NUMERIC:
+                            System.out.print(cell.getNumericCellValue() + "\t\t");
+                            break;
+                        case Cell.CELL_TYPE_STRING:
+                            System.out.print(cell.getStringCellValue() + "\t\t");
+                            break;
+                    }
+                }
+                System.out.println("");
+            }
+            file.close();
+            FileOutputStream out =
+                    new FileOutputStream(new File("C:\\test.xls"));
+            workbook.write(out);
+            out.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
 
